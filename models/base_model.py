0713bdefcde04046c7c324e000cd65eb9a32885c
Fab <f.nkurunziz@alustudent.com>
2023-05-20 16:30:04 +0200

    Starting Airbnb clone project

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.today()
+            self.updated_at = datetime.datetime.today()
 
     def __str__(self):
         """
