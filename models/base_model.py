d30a86e5dc696a59ded2d958ab5e971341c98714
Fab <f.nkurunziz@alustudent.com>
2023-05-20 16:26:03 +0200

    Starting Airbnb clone project

--- models/base_model.py
+++ models/base_model.py
@@ -14,14 +14,22 @@ class BaseModel:
         will inherit from
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
             this is an init method, this method will be called directly
             as we create a new instance of this BaseModel class
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.today()
-        self.updated_at = datetime.datetime.today()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = kwargs.get('id')
+                elif key == 'created_at' or key == 'updated_at':
+                    self.key = datetime.datetime.strptime(
+                        kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.today()
 
     def __str__(self):
         """
