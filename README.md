28d7abd13d74c62f3f4a67b4deb2461b248b7fed
Fab <f.nkurunziz@alustudent.com>
2023-05-20 12:19:37 +0200

    Starting Airbnb clone project

---
+++ README.md
@@ -0,0 +1,3 @@
+This project is about creating a clone of AirBnB website
+In this project which is The console, we will write the backend for the AirBnB website
+and also writing creating the console which will help us to interact with our backend before developing the frontent for our website using html and css together with JavaScript.
---
+++ models/base_model.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import uuid
+import datetime
+
+"""
+    imported uuid, and datetime modules
+"""
+
+
+class BaseModel:
+    """
+        this is the class that all other classes in this project
+        will inherit from
+    """
+
+    def __init__(self, id, created_at, updated_at):
+        """
+            this is an init method, this method will be called directly
+            as we create a new instance of this BaseModel class
+        """
+        self.id = str(uuid.uuid4)
+        self.created_at = datetime.datetime.today
+        self.updated_at = datetime.datetime.today
+
+    def __str__(self):
+        """
+            this method will print class name, id of an instance and a dictionary
+            representation of an instance
+        """
+        print(f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        """
+            this method will return the time we have updated our instance
+        """
+        self.updated_at = datetime.datetime.now
+
+    def to_dict(self):
+        """
+            this method will return the dictionary representation of
+            this instance
+        """
+        y = self.__dict__.copy()
+        y['__class__'] = self.__class__.__name__
+        y['created_at'] = self.created_at.isoformat()
+        y['updated_at'] = self.updated_at.isoformat()
+        return (y)
