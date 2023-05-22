9b15b71e823c2e035d16f60153ecdb8ebec2432c
Fab <f.nkurunziz@alustudent.com>
2023-05-22 12:49:40 +0200

    now we are saving our instances to a file

--- models/base_model.py
+++ models/base_model.py
@@ -20,6 +20,8 @@ class BaseModel:
             as we create a new instance of this BaseModel class
         """
         key = None
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'id':
