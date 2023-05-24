2d1a6dd9a14d9d5c98cfb5a469a94d4f7e5e2e4d
Fab <f.nkurunziz@alustudent.com>
2023-05-24 12:24:33 +0200

    AirBnB clone

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -3,8 +3,19 @@ from models.state import State
 
 
 class TestState(unittest.TestCase):
-    state = State()
-    state.name = "Rwanda"
-    state.assertIsInstance(state, State)
-    state.assertIsInstance(state.name, str)
-    state.assertEqual(state.name, "Rwanda")
+
+    def setUp(self):
+        # Create a sample State instance for testing
+        self.state = State()
+
+    def test_attribute(self):
+        # Test if the attribute is initialized correctly
+        self.assertEqual(self.state.name, '')
+
+    def test_attribute_type(self):
+        # Test if the attribute type is correct
+        self.assertIsInstance(self.state.name, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
