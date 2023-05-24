a15da31d7240bc19cfd478918144dcd0783eea9c
Fab <f.nkurunziz@alustudent.com>
2023-05-24 12:29:37 +0200

    AirBnB clone

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -3,11 +3,21 @@ from models.city import City
 
 
 class TestCity(unittest.TestCase):
-    city = City()
-    city.state_id = '121212'
-    city.name = "Kigali"
-    city.assertIsInstance(city, City)
-    city.assertIsInstance(city.state_id, str)
-    city.assertIsInstance(city.name, str)
-    city.assertEqual(city.state_id, "121212")
-    city.assertEqual(city.name, "Kigali")
+
+    def setUp(self):
+        # Create a sample City instance for testing
+        self.city = City()
+
+    def test_attributes(self):
+        # Test if the attributes are initialized correctly
+        self.assertEqual(self.city.state_id, '')
+        self.assertEqual(self.city.name, '')
+
+    def test_attribute_types(self):
+        # Test if the attribute types are correct
+        self.assertIsInstance(self.city.state_id, str)
+        self.assertIsInstance(self.city.name, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
