30a42311c0ad6654e2121f89ee3635906e341e99
Fab <f.nkurunziz@alustudent.com>
2023-05-25 12:27:37 +0200

    AirBnB clone

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,40 +1,20 @@
-#!/usr/bin/python3
-
 import unittest
 from models.amenity import Amenity
 
 
 class TestAmenity(unittest.TestCase):
 
-    def test_attributes(self):
-        amenity = Amenity()
-        self.assertEqual(amenity.name, '')
+    def setUp(self):
+        # Create a sample Amenity instance for testing
+        self.amenity = Amenity()
 
-    def test_name_setter(self):
-        amenity = Amenity()
-        amenity.name = 'Wifi'
-        self.assertEqual(amenity.name, 'Wifi')
+    def test_attribute(self):
+        # Test if the attribute is initialized correctly
+        self.assertEqual(self.amenity.name, '')
 
-    def test_to_dict(self):
-        amenity = Amenity()
-        amenity_dict = amenity.to_dict()
-        self.assertIsInstance(amenity_dict, dict)
-
-    # def test_from_dict(self):
-    #     amenity_dict = {'name': 'Wifi'}
-    #     amenity = Amenity(**amenity_dict)
-    #     self.assertEqual(amenity.name, 'Wifi')
-
-    def test_str_representation(self):
-        amenity = Amenity()
-        str_representation = str(amenity)
-        self.assertIn("'name': ''", str_representation)
-
-    def test_instance_creation_with_id(self):
-        amenity_dict = {'id': '123', 'name': 'Wifi'}
-        amenity = Amenity(**amenity_dict)
-        self.assertEqual(amenity.id, '123')
-        self.assertEqual(amenity.name, 'Wifi')
+    def test_attribute_type(self):
+        # Test if the attribute type is correct
+        self.assertIsInstance(self.amenity.name, str)
 
 
 if __name__ == '__main__':
