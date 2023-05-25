06dd2ff2d5940cbb9a1da2f444b05f6175e1e6dd
M-Aloysie <a.murekatet@alustudent.com>
2023-05-25 14:12:01 +0000

    Tasks Done

---
+++ models/place.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+defining class Place which will help us to create places
+in the city which has rooms which can be booked
+"""
+
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    this class has public class attributes which are:
+
+    city_id: string - empty string: it will be the City.id
+    user_id: string - empty string: it will be the User.id
+    name: string - empty string
+    description: string - empty string
+    number_rooms: integer - 0
+    number_bathrooms: integer - 0
+    max_guest: integer - 0
+    price_by_night: integer - 0
+    latitude: float - 0.0
+    longitude: float - 0.0
+    amenity_ids: list of string - empty list: it will be the\
+    list of Amenity.id later
+    """
+    city_id = ''
+    user_id = ''
+    name = ''
+    description = ''
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
